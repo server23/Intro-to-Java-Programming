54a96d63bc9d5b80747be422267819bce9bdf04b
server23 <roxana2389@yahoo.com>
2019-02-18 17:52:42 +0200

    Update Exercise_01_10.java

--- Exercise_01/Exercise_01_10/Exercise_01_10.java
+++ Exercise_01/Exercise_01_10/Exercise_01_10.java
@@ -5,6 +5,17 @@ that 1 mile is 1.6 kilometers.)
 */
 public class Exercise_01_10 {
 	public static void main(String[] args) {
-		System.out.println((14 / 45.30) / 1.6);
+		
+		/* 
+		--> 1 mile = 1.609344 km; 
+		    we need to transform the km in miles:
+		    14km / 1.609344 = A miles in 45.5 minutes 
+		    (45 minutes and 30 seconds is: 45.5 given that 1 minute is 60 seconds)
+	
+		--> we need to calculate the resulted miles for 60 minutes (miles per hour)
+		    (A * 60) / 45.5 
+		*/
+		
+		System.out.println(((14 /1.609344) * 60)/45.5); 
 	}
 }
